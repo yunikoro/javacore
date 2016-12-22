aa43fd4e65213e75ca2e087c98f2558a4e82cecb
yunikoro <542031180@qq.com>
2016-12-22 23:45:27 +0800

    Merge branch 'master' of https://github.com/yunikoro/javacore
    # Conflicts:
    #	javacore/src/com/javacore/exnd/OldEagle.java
    #	javacore/src/com/javacore/p1/Test.java

--- javacore/src/com/javacore/itfcImpl/Lion.java
+++ javacore/src/com/javacore/itfcImpl/Lion.java
@@ -3,6 +3,15 @@ package com.javacore.itfcImpl;
 import com.javacore.itfc.Walk;
 
 public class Lion implements Walk {
+	
+	private int age;
+	
+	
+	
+	public Lion(int age) {
+		super();
+		this.age = age;
+	}
 
 	@Override
 	public boolean isQuadruped() {
@@ -20,4 +29,18 @@ public class Lion implements Walk {
 		return true;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		Lion lion;
+		boolean isEqual;
+		if(obj instanceof Lion) {
+			System.out.println("this is lion");
+			lion = (Lion) obj;
+			if(lion.age == this.age)
+				return true;
+		}
+		return false;
+	}
+	
+	
 }
