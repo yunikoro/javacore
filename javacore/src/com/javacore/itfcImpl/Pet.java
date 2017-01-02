512a55c5ff19956f49a31f3f99ccb19ff4406592
cornelius huang <cornelius.huang@capgemini.com>
2017-01-02 15:15:54 +0800

    OCA P242

---
+++ javacore/src/com/javacore/itfcImpl/Pet.java
@@ -0,0 +1,22 @@
+package com.javacore.itfcImpl;
+
+public class Pet {
+	private int age;
+	protected String proCate;
+	public String category;
+		
+	public int getAge() {
+		return this.age;
+	}
+	public void setAge(int age) {
+		
+	}
+	
+	public Pet() {
+		
+	}
+	
+	public Pet(int age) {
+		this.age = age;
+	}
+}
---
+++ javacore/src/com/javacore/itfcImpl/Pig.java
@@ -0,0 +1,10 @@
+package com.javacore.itfcImpl;
+
+public class Pig extends Pet {
+	
+	private void roar() {
+		System.out.println("The "+getAge()+" years old lion says: \"Heng Heng\"");
+		System.out.println(category);
+		System.out.println(proCate);
+	}
+}
